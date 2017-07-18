7a808a4110b0ee9834929f80759236aade38a7c0
Yakeing <yakeing@gmail.com>
2017-07-18 14:57:18 +0800

    up 1.2

--- session.php
+++ session.php
@@ -1,35 +1,33 @@
 <?php
 /**
- * session class
- *@Author weibo.com/yakeing
- * 
- * set(name, value) Set up a session Value
- * get(name) Get a session Value
- * deletes(name) Write off a session Value
- * destroy() End all session Value
- * 
- *@ $expire Default server 180 minutes client end
- *@ $uid user ID, Default automatic generation
- *
- * 'session_name' Can customize session's name
- * SAE provides a solution for Memcache storage Session
- * 
- *PHP 5.3 Edition
- *  session_set_save_handler(
- *     array($handler, 'open'),
- *     array($handler, 'close'),
- *     array($handler, 'read'),
- *     array($handler, 'write'),
- *     array($handler, 'destroy'),
- *     array($handler, 'gc')
- * );
+  *  SESSION CLASS
+  *
+  * @author http://weibo.com/yakeing
+  * @version 1.2
+  * $expire Default server 180 minutes client end
+  * $uid user ID, Default automatic generation
+  *
+  * 'session_name' Can customize session's name
+  * SAE provides a solution for Memcache storage Session
+  *
+  *  PHP 5.3 Edition
+  *  session_set_save_handler(
+  *    array($handler, 'open'),
+  *    array($handler, 'close'),
+  *    array($handler, 'read'),
+  *    array($handler, 'write'),
+  *    array($handler, 'destroy'),
+  *    array($handler, 'gc')
+  * );
  */
 class session{
 	public $mcsession = false;
-	function __construct($expire = false, $uid = false){
-		if($this->mcsession){
+
+	//Initialization
+	function __construct($expire = false, $uid = false, $name = "MYSESSION"){
+		if($this->mcsession){//PHP 5.6 Edition
 			$handler = new sinacloud\sae\MemcacheSessionHandler();
-			session_set_save_handler($handler, true);//PHP 5.6 Edition
+			session_set_save_handler($handler, true);
 		}
 		session_cache_limiter('private');
 		if(is_int($expire)){
@@ -37,32 +35,40 @@ class session{
 			session_set_cookie_params($expire*60);
 		}
 		if(!empty($uid)) session_id($uid);
-		session_name("MYSESSION");
+		session_name($name);
 		session_start();
-	}
+	} //END __construct
 
-	function set($name, $value){
-		$_SESSION[$name] = $value;
-	}
+	//Set up a session Value
+	function Set($name, $value){
+		if(is_scalar($value)){
+			$_SESSION[$name] = $value;
+			return true;
+		}
+		return false;
+	} //END set
 
-	function get($name){
+	//Get a session Value
+	function Get($name){
 		if(isset($_SESSION[$name])){
 			return $_SESSION[$name];
 		}else{
-			return null;
+			return false;
 		}
-	}
+	} //END get
 
-	function deletes($name){
+	//Write off a session Value
+	function Deletes($name){
 		if(isset($_SESSION[$name])){
 			unset($_SESSION[$name]);
+			return true;
 		}
-	}
+		return false;
+	} //END deletes
 
-	function destroy(){
+	//End all session Value
+	//Use setcookie() Delete the client's SESSION ID
+	function Destroy(){
 		session_destroy();
-	}
-
-	function __destruct(){}
+	} //END destroy
 }
-?>
