ede421552a389fa097842ab59b4f25d29d6abb29
yakeing <yakeing@gmail.com>
2017-08-16 11:29:03 +0800

    add ob_start

--- tests/php_sessionTest/php_sessionTest.php
+++ tests/php_sessionTest/php_sessionTest.php
@@ -11,6 +11,7 @@ class php_sessionTest extends TestCase{
     $expire = 180;
     $uid = md5('uid');
     $name = "MYSESSION";
+    @ob_start(); //session_start(): Cannot send session cookie
     $session = new session($expire, $uid, $name);
     $name = 'admin';
     $value = 'pass';
