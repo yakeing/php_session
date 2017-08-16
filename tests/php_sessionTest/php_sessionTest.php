ad83de85e25e49b26cbacb1e94591ad97ae2a299
yakeing <yakeing@gmail.com>
2017-08-16 14:28:32 +0800

    nwe test session

--- tests/php_sessionTest/php_sessionTest.php
+++ tests/php_sessionTest/php_sessionTest.php
@@ -16,17 +16,17 @@ class php_sessionTest extends TestCase{
     $expire = 180;
     $uid = md5('uid');
     $name = "MYSESSION";
-    @ob_start(); //session_start(): Cannot send session cookie
+    //@ob_start(); //session_start(): Cannot send session cookie
     $session = new session($expire, $uid, $name);
     $name = 'admin';
     $value = 'pass';
     $set = $session->set($name, $value); //Set up a session Value
+    $this->assertTrue($set);
     $get = $session->get($name); //Get a session Value
+    $this->assertEquals($value, $get);
     $session->deletes($name); //Write off a session Value
     $getNull = $session->get($name); //Get a session Value
-    $this->assertTrue($set);
-    $this->assertEquals($value, $get);
-    $this->assertEquals(false, $getNull);
+    $this->assertFalse($getNull);
   }
 
 }
