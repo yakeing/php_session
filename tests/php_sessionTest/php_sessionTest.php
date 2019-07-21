5552a51775ae72b8d18f1e82afd7cdb670f412bc
Yakeing <yakeing@gmail.com>
2019-07-22 01:21:56 +0800

    Update php_sessionTest.php

--- tests/php_sessionTest/php_sessionTest.php
+++ tests/php_sessionTest/php_sessionTest.php
@@ -8,7 +8,7 @@ class php_sessionTest extends TestCase{
     $expire = 180;
     $uid = md5('uid');
     $name = "MYSESSION";
-    $session = new php_session\session($expire, $uid, $name);
+    $session = new \php_session\session($expire, $uid, $name);
     $name = 'admin';
     $value = 'pass';
     $set = $session->set($name, $value); //Set up a session Value
